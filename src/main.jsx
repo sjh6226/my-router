2c4ffcd5bde30afcf12713955b55befa46b34576
sjh <sjh6226@naver.com>
2025-07-02 12:21:02 +0900

    Nested Routes

--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,23 @@ import {
 } from "react-router-dom";
 import Root from "./routes/root"; // 루트 컴포넌트
 import ErrorPage from "./error-page"; // 에러 페이지
+import Contact from "./routes/contact";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
+  },
+  {
+    path: "contacts/:contactId",
+    element: <Contact />,
   },
 ]);
 
