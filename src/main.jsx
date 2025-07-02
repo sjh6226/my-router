69da5f3154b400a8819eaaef0ac67cc68a908a22
sjh <sjh6226@naver.com>
2025-07-02 12:35:23 +0900

    css적용 수정

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 // main.jsx
+import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import Root from "./routes/root"; // 루트 컴포넌트
-import ErrorPage from "./error-page"; // 에러 페이지
-import Contact from "./routes/contact";
+import Root from "./routes/root.jsx"; // 루트 컴포넌트
+import ErrorPage from "./error-page.jsx"; // 에러 페이지
+import Contact from "./routes/contact.jsx";
 
 const router = createBrowserRouter([
   {
