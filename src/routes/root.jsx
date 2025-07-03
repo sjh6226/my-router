0a0a398fc1d73411919603b220f499b7fb717bdc
sjh <sjh6226@naver.com>
2025-07-03 11:07:22 +0900

    Global Pending UI

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -4,6 +4,7 @@ import {
   useLoaderData,
   Form,
   redirect,
+  useNavigation,
 } from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
 
@@ -19,6 +20,7 @@ export async function loader() {
 
 export default function Root() {
   const { contacts } = useLoaderData();
+  const navigation = useNavigation();
   return (
     <>
       <div id="sidebar">
@@ -48,7 +50,7 @@ export default function Root() {
           </Form>
         </div>
         <nav>
-            {contacts.length ? (
+          {contacts.length ? (
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
@@ -57,8 +59,8 @@ export default function Root() {
                       isActive
                         ? "active"
                         : isPending
-                        ? "pending"
-                        : ""
+                          ? "pending"
+                          : ""
                     }
                   >
                     {contact.first || contact.last ? (
@@ -78,7 +80,7 @@ export default function Root() {
               <i>No contacts</i>
             </p>
           )}
-          
+
           {/* 
 
           <ul>
@@ -93,7 +95,11 @@ export default function Root() {
           */}
         </nav>
       </div>
-      <div id="detail">
+      <div id="detail"
+        className={
+          navigation.state === "loading" ? "loading" : ""
+        }
+      >
         <Outlet />
       </div>
     </>
